f9b37fbd02dd36511a472dc5df1e38293932303e
skyline75489 <skyline75489@outlook.com>
2016-04-09 14:44:09 +0800

    indent

--- hack2.js
+++ hack2.js
@@ -14,20 +14,20 @@ var touchGlow = null;
 var currentMousePosition = null;
 
 function onMouseDown(event) {
-	console.log('mouse down');
-	if (touchGlow) {
-		touchGlow.remove();
-	}
-	touchGlow = new Raster('red');
-	touchGlow.position = event.point;
+  console.log('mouse down');
+  if (touchGlow) {
+    touchGlow.remove();
+  }
+  touchGlow = new Raster('red');
+  touchGlow.position = event.point;
 }
 
 function onMouseDrag(event) {
-	console.log('mouse drag');
-	touchGlow.position = event.point;	
+  console.log('mouse drag');
+  touchGlow.position = event.point;
 }
 
 function onMouseUp(event) {
-	console.log('mouse up');
-	touchGlow.remove();
+  console.log('mouse up');
+  touchGlow.remove();
 }
