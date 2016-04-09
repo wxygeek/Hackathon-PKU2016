ff869288ea5507a5befc274318b67e02b559c71d
skyline75489 <skyline75489@outlook.com>
2016-04-09 14:37:46 +0800

    touch glow

--- hack2.js
+++ hack2.js
@@ -8,5 +8,26 @@ var myCircle = new Path.Circle(new Point(100, 70), 50);
 myCircle.strokeColor = 'black';
 myCircle.selected = true;
 
-myCircle.removeSegment(2);
-myCircle.add(new Point(200, 20));
+
+var touchGlow = null;
+
+var currentMousePosition = null;
+
+function onMouseDown(event) {
+	console.log('mouse down');
+	if (touchGlow) {
+		touchGlow.remove();
+	}
+	touchGlow = new Raster('red');
+	touchGlow.position = event.point;
+}
+
+function onMouseDrag(event) {
+	console.log('mouse drag');
+	touchGlow.position = event.point;	
+}
+
+function onMouseUp(event) {
+	console.log('mouse up');
+	touchGlow.remove();
+}
